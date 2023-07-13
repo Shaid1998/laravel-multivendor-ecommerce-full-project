172caf48a57bab75bbc71cf655f63f153e0c8906
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-07-14 04:22:44 +0600

    14-07-2023

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class VendorController extends Controller
 {
@@ -11,4 +15,20 @@ class VendorController extends Controller
 
         return view('vendor.index');
     }//end method
+
+    public function VendorLogin(){
+        return view('vendor.vendor_login');
+    } // End Mehtod 
+
+
+
+public function VendorDestroy(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/vendor/login');
+    } // End Mehtod
 }
