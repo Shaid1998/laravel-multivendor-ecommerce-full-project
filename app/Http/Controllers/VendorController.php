b4f9fccd41cf2d2267fdceddf1d3fa803c837db6
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-07-14 05:16:13 +0600

    14-07-2023

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -2,30 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+ 
+use App\Notifications\VendorRegNotification;
+use Illuminate\Support\Facades\Notification;
 
+ 
 class VendorController extends Controller
 {
     public function VendorDashboard(){
 
         return view('vendor.index');
-    }//end method
 
-    public function VendorLogin(){
+    } // End Mehtod 
+
+  public function VendorLogin(){
         return view('vendor.vendor_login');
     } // End Mehtod 
-    public function VendorProfile(){
-
-        $id = Auth::user()->id;
-        $vendorData = User::find($id);
-        return view('vendor.vendor_profile_view',compact('vendorData'));
-
-    } // End Mehtod
 
 
 
@@ -37,9 +33,20 @@ public function VendorDestroy(Request $request){
         $request->session()->regenerateToken();
 
         return redirect('/vendor/login');
-    } // End Mehtod
+    } // End Mehtod 
 
-    public function VendorProfileStore(Request $request){
+
+
+public function VendorProfile(){
+
+        $id = Auth::user()->id;
+        $vendorData = User::find($id);
+        return view('vendor.vendor_profile_view',compact('vendorData'));
+
+    } // End Mehtod 
+
+
+     public function VendorProfileStore(Request $request){
 
         $id = Auth::user()->id;
         $data = User::find($id);
@@ -70,4 +77,76 @@ public function VendorDestroy(Request $request){
 
     } // End Mehtod 
 
+
+
+  public function VendorChangePassword(){
+        return view('vendor.vendor_change_password');
+    } // End Mehtod 
+
+
+
+public function VendorUpdatePassword(Request $request){
+        // Validation 
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed', 
+        ]);
+
+        // Match The Old Password
+        if (!Hash::check($request->old_password, auth::user()->password)) {
+            return back()->with("error", "Old Password Doesn't Match!!");
+        }
+
+        // Update The new password 
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password)
+
+        ]);
+        return back()->with("status", " Password Changed Successfully");
+
+    } // End Mehtod 
+
+
+
+    public function BecomeVendor(){
+        return view('auth.become_vendor');
+    } // End Mehtod 
+
+
+      public function VendorRegister(Request $request) {
+       
+        $vuser = User::where('role','admin')->get();
+
+
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed'],
+        ]);
+
+        $user = User::insert([ 
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'vendor_join' => $request->vendor_join,
+            'password' => Hash::make($request->password),
+            'role' => 'vendor',
+            'status' => 'inactive',
+        ]);
+
+          $notification = array(
+            'message' => 'Vendor Registered Successfully',
+            'alert-type' => 'success'
+        );
+
+       Notification::send($vuser, new VendorRegNotification($request));
+        return redirect()->route('vendor.login')->with($notification);
+       
+    }// End Mehtod 
+
+
+
+
 }
+ 
