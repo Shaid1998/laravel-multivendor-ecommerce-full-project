c14b2fa49dfd1cce71a95726bca326000912f85d
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-07-13 18:43:46 +0600

    13-07-2023

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,11 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
 
         return view('admin.index');
+    }//end method
+
+    public function AdminLogin(){
+        return view('admin.admin_login');
+    }
+
+    /**
+     * Destroy an authenticated admin session.
+     */
+    public function AdminDestroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
     }//end method
 }
