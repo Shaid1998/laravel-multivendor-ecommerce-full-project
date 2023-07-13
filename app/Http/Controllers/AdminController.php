f99b2175c1fdce624f9feaa68ca9b903f1db5169
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-07-13 19:23:02 +0600

    13-07-2023

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -16,7 +17,7 @@ class AdminController extends Controller
 
     public function AdminLogin(){
         return view('admin.admin_login');
-    }
+    }//end method
 
     /**
      * Destroy an authenticated admin session.
@@ -30,5 +31,12 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/admin/login');
+    }//end method
+
+    public function AdminProfile(){
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+
+        return view('admin.admin_profile_view', compact('adminData'));
     }//end method
 }
